fd806e8c7e5ff28af63479d00fb2a4cce37aa3fd
sabane <sabane@LIN13003519.corp.capgemini.com>
2018-03-05 21:27:58 +0530

    blank lines removed

--- First_Java/src/sam/day1/HelloWorld.java
+++ First_Java/src/sam/day1/HelloWorld.java
@@ -6,14 +6,9 @@ import java.util.logging.Logger;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
 		Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-		
 		logger.log(Level.INFO, "Hello World!!!");
-		
-					
-	}
-	
+		}
 }
 
 
