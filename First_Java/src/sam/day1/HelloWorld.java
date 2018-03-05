76e19aee50324957099ea37a63cc5bbc604bb014
sabane <sabane@LIN13003519.corp.capgemini.com>
2018-03-05 21:24:39 +0530

    Logger utility used as a best practice instead of system.print

--- First_Java/src/sam/day1/HelloWorld.java
+++ First_Java/src/sam/day1/HelloWorld.java
@@ -1,13 +1,15 @@
 package sam.day1;
 
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		
+		Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 		
-		System.out.println("Hello world !!");
+		logger.log(Level.INFO, "Hello World!!!");
 		
 					
 	}
